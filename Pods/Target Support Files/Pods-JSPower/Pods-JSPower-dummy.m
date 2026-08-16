c360a2b7a2094d3fb45711d8698e89aa5b0442ae
Everett <everettjf@live.com>
2026-08-16 01:03:59 -0700

    Remove legacy CocoaPods and Objective-C implementation

--- Pods/Target Support Files/Pods-JSPower/Pods-JSPower-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JSPower : NSObject
-@end
-@implementation PodsDummy_Pods_JSPower
-@end
--- Pods/Target Support Files/Pods-JSPowerExtension/Pods-JSPowerExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JSPowerExtension : NSObject
-@end
-@implementation PodsDummy_Pods_JSPowerExtension
-@end
